b7828ae3ec37c091705bb94789c9c7c8335a3b68
Дмитрий Борковский <97740709+hlyfcknsht@users.noreply.github.com>
2024-10-19 23:03:38 +0300

    Update traffic_light.py
    Изменена логика работы без привязки ко времени, оставил привязку к интервалу

--- traffic_light.py
+++ traffic_light.py
@@ -1,27 +1,16 @@
 import sys
-import time
 
-M = int(sys.argv[1])  # Длительность зеленого света
-N = int(sys.argv[2])  # Длительность красного света
-T = int(sys.argv[3])  # Время, в которое нужно определить состояние светофора
+N = int(sys.argv[1])
+M = int(sys.argv[2])
+T = int(sys.argv[3])
 
-# Определяем цикл светофора
-cycle_duration = M + N
 
-# Определяем момент времени внутри текущего цикла
-current_time_in_cycle = T % cycle_duration
+cycle_length = N + M  # Длина полного цикла
+time_in_cycle = T % cycle_length  # Время в текущем цикле
 
-if M > N:
-    if current_time_in_cycle <= M:
-        print("Green")
-    else:
-        print("Red")
-elif N > M:
-    if current_time_in_cycle >= M:
-        print("Green")
-    else:
-        print("Red")
+if time_in_cycle < N:
+    print("green")
 else:
-    print('Yellow')
+    print("red")
 
 
